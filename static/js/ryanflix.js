68250f5b7263fc95ae1b5f9ddd132db2a575ed98
Ryan Asher <ryandeanasher@gmail.com>
2016-05-16 20:30:34 -0400

    Update button click function name, and set up some alerts for testing.

--- static/js/ryanflix.js
+++ static/js/ryanflix.js
@@ -1,20 +1,24 @@
-function showRandomEpisode() {
-	// var selected_show = document.getElementsByClassName('shows')[0].value;
-	// var data = 'data/' + selected_show + '.json';
+function retrieveEpisodes() {
+	var json_file =
+		'/data/' + document.getElementsByClassName('shows')[0].value + '.json';
 	var httpRequest = new XMLHttpRequest;
 
 	if (!httpRequest) {
-		console.log('Can\t create an XMLHTTP Request.');
+		console.log('Can\'t create an XMLHTTP Request.');
 		return false;
 	}
 
 	httpRequest.overrideMimeType('application/json');
-	httpRequest.open('GET', chrome.extensiondata, true);
+	httpRequest.open('GET', chrome.extension.getURL(json_file), true);
 	httpRequest.onreadystatechange = function() {
 		if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-			callback(alert(httpRequest.responseText));
+			showRandomEpisode(httpRequest.responseText);
 		}
 	}
 	httpRequest.send(null);
 }
 
+function showRandomEpisode(episodes) {
+	alert("WE HERE.");
+	alert(showRandomEpisode);
+}
